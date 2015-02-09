9aa1dda1a19d8a538df2833145d10fc7972929c7
Nico <nico.vijlbrief@outlook.com>
2015-02-09 17:34:40 +0100

    Added VS C# console crawlers
    Made this to work in C#, love it more

---
+++ XbeScan/XbeScan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XbeScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Out.WriteLine("Xbe Scan requires a filelist file");
+                Console.ReadKey();
+            }
+            else
+            {
+
+
+                if (args[0] == "test.txt") { }
+
+                Console.Out.WriteLine("Reading " + args[0]);
+                switch (args[0])
+                {
+                    default:
+
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
