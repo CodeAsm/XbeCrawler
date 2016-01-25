4ba9b7548580e4f4e90cf8cd8975a1e4583c5dc0
CodeAsm <nico.vijlbrief@outlook.com>
2016-01-25 12:10:51 +0100

    Added objControll library for universal usage. Argument control prototype

--- XbeScan/XbeScan/Program.cs
+++ XbeScan/XbeScan/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using objControl;
+using System;
+using System.IO;
 
 namespace XbeScan
 {
@@ -13,6 +11,17 @@ namespace XbeScan
             if (args.Length == 0)
             {
                 Console.Out.WriteLine("Xbe Scan requires a filelist file");
+                Console.Out.WriteLine("Multiply 2 X 7 =" + bin2Obj.Multiply(2, 7));
+                string file = "VIVA";
+                if (File.Exists(file))
+                {
+                    BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open));
+
+                    byte[] blob = bin2Obj.getByteFullArray(reader.BaseStream);
+                    Console.Out.WriteLine("length of " + file + " is: " + blob.LongLength.ToString() + " bytes");
+                    Console.Out.WriteLine("first 16 bytes : " + readBytes(blob, 16));
+
+                }
                 Console.ReadKey();
             }
             else
@@ -33,5 +42,15 @@ namespace XbeScan
             }
         }
 
+        private static string readBytes(byte[] blob, int v)
+        {
+            byte[] rValue = new byte[1 * v];
+            for (int i = 0; i < v; i++)
+            {
+                rValue[i] = blob[i];
+            }
+            return System.Text.Encoding.UTF8.GetString(rValue);
+              
+        }
     }
 }
---
+++ XbeScan/objControl/bin2obj.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace objControl
+{
+    public class bin2Obj
+    {
+        /// <summary>
+        /// Demo function, multiply x with y.
+        /// TODO: remove after basic functions added
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static long Multiply(long x, long y)
+        {
+            return (x * y);
+        }
+
+        public static byte[] getByteFullArray(Stream input) {
+            ///http://www.yoda.arachsys.com/csharp/readbinary.html
+            /// http://stackoverflow.com/questions/221925/creating-a-byte-array-from-a-stream
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
